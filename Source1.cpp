e89af65ea69c68426f087e96f3f3fb684243ec3f
Henry <mykim1891@hotmail.com>
2020-06-22 19:20:07 -0700

    Created Asset (tetriminos)

---
+++ Source1.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+wstring tetromino[7];
+
+int main() {
+	// Create assets
+	tetrominos[0].append(L"..X.");
+	tetrominos[0].append(L"..X.");
+	tetrominos[0].append(L"..X.");
+	tetrominos[0].append(L"..X.");
+
+	tetrominos[1].append(L"..X.");
+	tetrominos[1].append(L".XX.");
+	tetrominos[1].append(L".X..");
+	tetrominos[1].append(L"....");
+
+	tetrominos[2].append(L".X..");
+	tetrominos[2].append(L".XX.");
+	tetrominos[2].append(L"..X.");
+	tetrominos[2].append(L"....");
+
+	tetrominos[3].append(L"....");
+	tetrominos[3].append(L".XX.");
+	tetrominos[3].append(L".XX.");
+	tetrominos[3].append(L"....");
+
+	tetrominos[4].append(L"..X.");
+	tetrominos[4].append(L".XX.");
+	tetrominos[4].append(L"..X.");
+	tetrominos[4].append(L"....");
+
+	tetrominos[5].append(L"....");
+	tetrominos[5].append(L".XX.");
+	tetrominos[5].append(L"..X.");
+	tetrominos[5].append(L"..X.");
+
+	tetrominos[6].append(L"....");
+	tetrominos[6].append(L".XX.");
+	tetrominos[6].append(L".X..");
+	tetrominos[6].append(L".X..");
+
+}
